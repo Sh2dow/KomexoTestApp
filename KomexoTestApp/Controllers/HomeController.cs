440af92cfef2dda54ed2c6051ce52aa5d88211ee
sh2dow <sh2dow@PX-DESKTOP>
2017-04-08 20:56:52 +0300

    Ready

---
+++ KomexoTestApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using KomexoTestApp.Models;
+using MvcRazorToPdf;
+using System.Web.Mvc;
+using System.IO;
+using System.Drawing;
+using System;
+using System.Drawing.Imaging;
+
+namespace KomexoTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult GeneratePDF(KomexoOrder model)
+        {
+            var img = Base64ToImage(model.Signature);
+            img.Save(Server.MapPath("~") + "/Resources/Signature.png", ImageFormat.Png);
+            return new PdfActionResult(model);
+        }
+
+        public Image Base64ToImage(string base64String)
+        {
+            // Convert Base64 String to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0,
+              imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+
+    }
+}
---
+++ KomexoTestApp/Models/KomexoOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KomexoTestApp.Models
+{
+    public class KomexoOrder
+    {
+        public int OrderNr { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime ServiceDate { get; set; }
+        public string EngineerName { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string ToDo { get; set; }
+        public int Cable { get; set; }
+        public int Plate { get; set; }
+        public int Screw { get; set; }
+        public string Signature { get; set; }
+    }
+}
